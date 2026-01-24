948186dd93c353cc0dcdc721ccf7978742344de2
Tim <timkentdev@gmail.com>
2026-01-24 15:54:38 -0800

    refactor: define express app in app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,5 @@
-import express from 'express';
+import app from './app.js';
 import config from './config/config.js';
-import { defaultErrorHandler, routeNotFound } from './middleware/errors.middleware.js';
-
-const app = express();
-
-app.use(routeNotFound);
-app.use(defaultErrorHandler);
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
