f4fd0c14260411e7b5ed56214cb413454fe7fb1e
Tim <timkentdev@gmail.com>
2026-01-24 13:09:23 -0800

    chore: add mongo config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,17 @@
 import 'dotenv/config';
 
 interface Config {
+	mongoCluster: string;
+	mongoUser: string;
+	mongoPassword: string;
 	port: number;
 }
 
 const config: Config = {
-	port: Number(process.env.PORT),
+	mongoCluster: process.env.MONGODB_CLUSTER!,
+	mongoPassword: process.env.MONGODB_PASSWORD!,
+	mongoUser: process.env.MONGODB_USERNAME!,
+	port: Number(process.env.PORT!),
 };
 
 export default config;
