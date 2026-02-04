9a91dd4dce09f0b486731c9359950917b48a60a2
Tim <timkentdev@gmail.com>
2026-02-04 12:57:05 -0800

    add route tests

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -7,6 +7,9 @@ vi.mock('../services/homes.services.ts', () => {
 		getHomes: vi.fn(() => {
 			return [{ id: 1 }];
 		}),
+		getHome: vi.fn(() => {
+			return { id: 1 };
+		}),
 	};
 });
 
@@ -29,10 +32,29 @@ describe('Homes route', () => {
 	});
 
 	describe('GET /homes/:id', async () => {
-		it('sends 400 error if id is not a number', async () => {});
+		it('sends 404 error if id is not a number', async () => {
+			return request(app).get('/homes/bad-route').expect(400);
+		});
 
-		it('sends 400 error if id is not a valid number', async () => {});
+		it('sends 400 error if id is not a valid number', async () => {
+			const res1 = await request(app).get('/homes/-1');
+			expect(res1.status).toBe(400);
 
-		it('sends the correct home document for a valid id', async () => {});
+			const res2 = await request(app).get('/homes/13');
+			expect(res2.status).toBe(400);
+
+			const res3 = await request(app).get('/homes/31');
+			expect(res3.status).toBe(400);
+		});
+
+		it('sends the correct home document for a valid id', async () => {
+			return request(app)
+				.get('/homes/1')
+				.expect(200)
+				.expect('Content-Type', /json/)
+				.then((res) => {
+					expect(res.body).toEqual({ id: 1 });
+				});
+		});
 	});
 });
