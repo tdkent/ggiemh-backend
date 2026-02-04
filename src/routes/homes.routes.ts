cfca3726a791fbf46825c2b3bf522e84619d150d
Tim <timkentdev@gmail.com>
2026-02-04 13:32:00 -0800

    extend express Request type

--- src/routes/homes.routes.ts
+++ src/routes/homes.routes.ts
@@ -1,24 +1,26 @@
+import type { Request } from 'express';
 import express from 'express';
+import type { Document, WithId } from 'mongodb';
+import checkValidId from '@/helpers/validate.js';
 import { getHome, getHomes } from '@/services/homes.services.js';
 
 const router = express.Router();
+
+interface TypedRequest extends Request {
+	home?: WithId<Document> | null;
+}
 
 router.get('/', async (_req, res) => {
 	const homes = await getHomes();
 	res.send(homes);
 });
 
-router.param('id', async (req, res, next, id) => {
-	const validId = Number(id);
+router.param('id', async (req: TypedRequest, res, next, id) => {
+	const validId = checkValidId(id);
 
-	if (
-		!validId ||
-		validId < 1 ||
-		validId > 30 ||
-		validId === 13 ||
-		validId === 22
-	) {
+	if (!validId) {
 		res.status(400).send('Invalid request');
+		return;
 	}
 
 	const home = await getHome(validId);
@@ -26,7 +28,7 @@ router.param('id', async (req, res, next, id) => {
 	next();
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req: TypedRequest, res) => {
 	res.send(req.home);
 });
 
