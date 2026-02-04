2d40458348d36401d5b518b5ccd2131bdc3bdcf8
Tim <timkentdev@gmail.com>
2026-02-04 13:04:59 -0800

    add param and GET router methods

--- src/routes/homes.routes.ts
+++ src/routes/homes.routes.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import { getHomes } from '@/services/homes.services.js';
 
-const homesRouter = express.Router();
+const router = express.Router();
 
-homesRouter.get('/', async (_req, res) => {
+router.get('/', async (_req, res) => {
 	const homes = await getHomes();
 	res.send(homes);
 });
 
-export default homesRouter;
+router.param('/:id', async (req, res, next, id) => {
+	console.log(id);
+	// validate id
+	// fetch home
+	// call getHome()
+	// add home to req
+	next();
+});
+
+router.get('/:id', async (req, res) => {
+	res.send('hello world');
+});
+
+export default router;
