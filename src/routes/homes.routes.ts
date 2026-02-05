1fb8249cb6a3a41414b280694d86dc98843b321e
Tim <timkentdev@gmail.com>
2026-02-04 16:18:51 -0800

    send error or home

--- src/routes/homes.routes.ts
+++ src/routes/homes.routes.ts
@@ -1,35 +1,25 @@
-import type { Request } from 'express';
 import express from 'express';
-import type { Document, WithId } from 'mongodb';
+import createHttpError from 'http-errors';
 import checkValidId from '@/helpers/validate.js';
 import { getHome, getHomes } from '@/services/homes.services.js';
 
 const router = express.Router();
-
-interface TypedRequest extends Request {
-	home?: WithId<Document> | null;
-}
 
 router.get('/', async (_req, res) => {
 	const homes = await getHomes();
 	res.send(homes);
 });
 
-router.param('id', async (req: TypedRequest, res, next, id) => {
-	const validId = checkValidId(id);
+router.get('/:id', async (req, res, next) => {
+	const { id } = req.params;
+	const validId = checkValidId(id as string);
 
 	if (!validId) {
-		res.status(400).send('Invalid request');
-		return;
+		return next(createHttpError(400, 'Invalid request'));
 	}
 
 	const home = await getHome(validId);
-	req.home = home;
-	next();
-});
-
-router.get('/:id', (req: TypedRequest, res) => {
-	res.send(req.home);
+	res.send(home);
 });
 
 export default router;
